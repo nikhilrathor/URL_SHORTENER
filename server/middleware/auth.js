cd2ffefe36aaef89e657481af9dda2719d14facb
nikhilrathor <nikhilrathor01@gmail.com>
2020-07-23 20:26:48 +0530

    user and url routes added with auth middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,18 @@
+const config = require('../config/key');
+const jwt = require('jsonwebtoken');
+
+function auth(req, res, next) {
+    const token = req.header('x-auth-token');
+
+    if (!token) return res.status(401).json({ msg: "No token, authorization denied" });
+
+    try {
+        const decoded = jwt.verify(token, config.jwtSecret);
+        req.user = decoded;
+        next();
+    } catch (e) {
+        res.satus(400).json({ msg: "token is not valid" });
+    }
+}
+
+module.exports = auth;
