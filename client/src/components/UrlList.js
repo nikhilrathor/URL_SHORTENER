bcaadef44bbe904dbf8638ca66408ed449a94792
nikhilrathor <nikhilrathor01@gmail.com>
2020-07-25 13:02:22 +0530

    newUser message added

--- client/src/components/UrlList.js
+++ client/src/components/UrlList.js
@@ -17,33 +17,38 @@ class UrlList extends Component {
 
     render() {
         const { urls } = this.props.url;
+        let newUser = '';
+        if (urls.length === 0) {
+            newUser = "Start creating your short Urls!";
+        }
         return (
             <Container>
-                <Table hover responsive className="mt-5 text-center">
-                    <thead>
-                        <tr>
-                            <th>Delete</th>
-                            <th>Full Url</th>
-                            <th>Short Url</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {urls.map(({ _id, FullUrl, ShortUrl }) => (
-                            <tr key={_id}>
-                                <td>
-                                    <Button
-                                        className="remove-btn"
-                                        color="danger"
-                                        size="sm"
-                                        onClick={this.onDeleteClick.bind(this, _id)}
-                                    >&times;</Button>
-                                </td>
-                                <td><NavLink href={FullUrl}>{FullUrl}</NavLink></td>
-                                <td><NavLink href={ShortUrl}>{ShortUrl}</NavLink></td>
+                {newUser === '' ?
+                    <Table hover responsive className="mt-5 text-center">
+                        <thead>
+                            <tr>
+                                <th>Delete</th>
+                                <th>Full Url</th>
+                                <th>Short Url</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </Table>
+                        </thead>
+                        <tbody>
+                            {urls.map(({ _id, FullUrl, ShortUrl }) => (
+                                <tr key={_id}>
+                                    <td>
+                                        <Button
+                                            className="remove-btn"
+                                            color="danger"
+                                            size="sm"
+                                            onClick={this.onDeleteClick.bind(this, _id)}
+                                        >&times;</Button>
+                                    </td>
+                                    <td><NavLink href={FullUrl}>{FullUrl}</NavLink></td>
+                                    <td><NavLink href={ShortUrl}>{ShortUrl}</NavLink></td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table> : <h3 className="text-center">{newUser}</h3>}
             </Container>
         );
     }
